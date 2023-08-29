d81282c52944010bb32200830b21c35491132caa
Pruthvi Hosmani <67551298+pruthvihosmani@users.noreply.github.com>
2023-08-29 09:13:54 +0000

    index.js updated

--- part-number-kiosk/src/scrpit.js
+++ part-number-kiosk/src/scrpit.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 
 // Replace 'your-local-uri' with the connection string to your local MongoDB instance
-const uri = 'mongodb://localhost:27017/RBOSCH.PUMPNUMBERS';
+const uri = const uri = 'mongodb://localhost:27017/';
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
